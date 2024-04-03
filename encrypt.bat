32179b0f58d7e180f0b9c9d3389dff863000e637
pwnsource <165674531+pwnsource@users.noreply.github.com>
2024-04-03 19:43:58 +0300

    Add files via upload

---
+++ encrypt.bat
@@ -0,0 +1,27 @@
+@echo off
+
+REM Check if Flask is installed
+pip show Flask >nul 2>&1
+if errorlevel 1 (
+    REM Flask is not installed, install it
+    pip install Flask
+)
+
+REM Check if openpyxl is installed
+pip show openpyxl >nul 2>&1
+if errorlevel 1 (
+    REM openpyxl is not installed, install it
+    pip install openpyxl
+)
+
+REM Check if cryptography is installed
+pip show cryptography >nul 2>&1
+if errorlevel 1 (
+    REM cryptography is not installed, install it
+    pip install cryptography
+)
+
+REM Run the Python script
+python encrypt.py
+
+pause
